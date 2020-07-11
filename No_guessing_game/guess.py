2ada8615ef6f693a3631bcd1422be44d479680bb
adityaVelhal <58719424+adityaVelhal@users.noreply.github.com>
2020-07-11 23:10:58 +0530

    Update guess.py

--- No_guessing_game/guess.py
+++ No_guessing_game/guess.py
@@ -4,19 +4,21 @@ import random
 def play():
     guess_used = 0
     name = input("Your Name : ")
-    number = random.randint(1, 5)
-    print(number)
+    number = random.randint(1, 50)
+    
     print('\nHello ' + name + ', Welcome to number guessing game...😁😁')
-    print('\nI am guessing no. between 1 to 5...')
+    print('\nI am guessing no. between 1 to 50...')
     print('\nYou have 5 attempts to guess it right!')
 
     while guess_used < 5:
         guess_used += 1
         guess_no = int(input("\nWhat's your guess: "))
+        
         if number == guess_no:
             print('\nGreat! You guessed it right.')
             print("Better luck next time.\n")
             response = int(input("Press 1 to play again or 0 to end : "))
+            
             if response == 1:
                 play()
             else:
